55b9c839cf2472a94a4fe9370f60c333987ba62b
Will Brinkman <willbkman@gmail.com>
2023-08-31 15:51:40 -0500

    Integrated API calls, user authentication, post management,a messaging view, and enhanced error handling

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,17 +1,27 @@
-export default function Post({
+import { useContext, useState } from "react";
+import { UserContext } from "../contexts/UserContext";
+import MessageForm from "./MessageForm";
+
+function Post({
   _id: id,
   author,
   description,
-  isAuthor,
   location,
-  message,
+  messages,
   price,
   title,
   willDeliver,
   active,
+  handleDelete,
 }) {
+  const { user } = useContext(UserContext);
+  const [showMessages, setShowMessages] = useState(false);
+
+  const canSendMessage = user && user.username !== author.username;
+  const isAuthor = user && user.username === author.username;
+
   return (
-    <section key={id} className={`post ${active ? "active" : ""}`}>
+    <section key={id} className={`container ${active ? "active" : ""}`}>
       <h2>{title}</h2>
       <p>
         <strong>Author:</strong> {author.username}
@@ -29,16 +39,34 @@ export default function Post({
         <strong>Will Deliver:</strong> {willDeliver ? "Yes" : "No"}
       </p>
 
-      {isAuthor && <p>This is your post!</p>}
-
-      {/* If there's a message for the post, show it */}
-      {message && (
+      {isAuthor && (
         <>
-          <h3>Message:</h3>
-          <p>{message}</p>
+          <p>This is a post you made.</p>
+          <button className="danger-button"onClick={() => handleDelete(id)}>Delete Post</button>
+          {messages && messages.length > 0 ? (
+            <>
+              <button onClick={() => setShowMessages(!showMessages)}>
+                {showMessages ? "Hide Messages" : "Show Messages"}
+              </button>
+              {showMessages &&
+                messages.map((message) => (
+                  <div key={message._id}>
+                    <p>From: {message.fromUser.username}</p>
+                    <p>Message: {message.content}</p>
+                  </div>
+                ))}
+            </>
+          ) : (
+            <p>No messages 😔</p>
+          )}
         </>
       )}
+
+      {canSendMessage && <MessageForm postId={id} loggedInUser={user} />}
+
       {!active && <p>This post has been deleted.</p>}
     </section>
   );
 }
+
+export default Post;
